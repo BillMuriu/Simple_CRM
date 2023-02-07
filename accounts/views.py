221a1210fdd6aa68b6bdfd318bc49c8fd7609374
unknown <njorogekamau63@gmail.com>
2023-02-07 19:11:54 +0300

    Adding the inbuilt django register form

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import *
 from .forms import OrderForm
 from .filters import OrderFilter
 
-# Create your views here.
-from .models import *
+
 
 def registerPage(request):
-    context = {}
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
